888cae19574ea5324fc73aab8bfea1b8fe8fb507
vitorfloriano <vitorflorianoc@gmail.com>
2023-03-16 00:18:24 -0300

    I think I'm starting to get it. Kind of.

---
+++ Topics/Invoking functions/Lembas/src/Main.kt
@@ -0,0 +1,14 @@
+// This is the totalLembas() function. It just counts the total number of lembas. 
+// Do not change this code
+fun totalLembas(first: String, second: String) {
+    print(first.toInt() + second.toInt())
+}
+
+fun main() {
+    val breadFromFrodo = readln()
+    val breadFromSam = readln()
+
+    // write your code here
+totalLembas(breadFromFrodo,breadFromSam)
+
+}
