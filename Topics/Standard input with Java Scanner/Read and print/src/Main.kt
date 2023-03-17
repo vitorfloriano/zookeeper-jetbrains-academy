a0e4786c13f9609ebfbd9004097ba8a9befbf537
vitorfloriano <vitorflorianoc@gmail.com>
2023-03-16 23:57:21 -0300

    Just practicing a bit.

---
+++ Topics/Standard input with Java Scanner/Read and print/src/Main.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val var1 = scanner .nextLine()
+    val var2 = scanner .nextLine()
+    val var3 = scanner .nextInt()
+    val var4 = scanner .nextInt()
+
+    println(var1)
+    println(var2)
+    println(var3)
+    println(var4)
+
+}
