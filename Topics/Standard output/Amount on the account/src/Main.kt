5366a978fe4822fa4b8ed88b8f3ea067ae6c603d
vitorfloriano <vitorflorianoc@gmail.com>
2023-03-18 15:37:02 -0300

    Just practicing a bit.

---
+++ Topics/Standard output/Amount on the account/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("""
+        08:00 You have one billion dollars
+        08:30 You have two thousand dollars
+        09:10 You have no dollars!
+        """.trimIndent())
+}
