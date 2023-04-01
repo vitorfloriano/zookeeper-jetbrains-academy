0bc9eb2fdddd2897e0e4bf68e9072d966230152e
vitorfloriano <vitorflorianoc@gmail.com>
2023-03-31 21:24:26 -0300

    Just practicing a bit.

--- Topics/Introduction to MutableList/Initializing a list/src/Main.kt
+++ Topics/Introduction to MutableList/Initializing a list/src/Main.kt
@@ -1,8 +1,9 @@
 fun main() {
-    val numbers = mutableListOf(100) { 0 }
+    val numbers = MutableList(100, { 0 })
     numbers[0] = 1
     numbers[9] = 10
     numbers[99] = 100
     // do not touch the lines below
     println(numbers.joinToString())
 }
+
---
+++ Topics/Introduction to MutableList/Initializing a mutable list of longs/src/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val longs = mutableListOf<Long>(100_000_000_001, 100_000_000_002, 100_000_000_003)
+    // do not touch the lines below
+    println(longs.joinToString())
+}
