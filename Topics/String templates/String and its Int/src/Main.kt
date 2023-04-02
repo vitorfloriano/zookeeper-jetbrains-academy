8a575bc2e543fad504b2163158eda116ed337700
vitorfloriano <vitorflorianoc@gmail.com>
2023-04-02 19:25:01 -0300

    Just practicing a bit.

---
+++ Topics/String templates/String and its Int/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val strnum = readln()
+    val num = strnum.toInt()
+    val quotient = num / 2
+    println("The obtained value is $strnum and its Int representation after division by 2 is $quotient")
+}
