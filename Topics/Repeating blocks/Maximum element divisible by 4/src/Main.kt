08893d0e8de06f530b9d686274a241322423325c
vitorfloriano <vitorflorianoc@gmail.com>
2023-04-07 19:52:34 -0300

    Just practicing a bit.

--- Topics/Repeating blocks/Maximum element divisible by 4/src/Main.kt
+++ Topics/Repeating blocks/Maximum element divisible by 4/src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-
+    val n = readln().toInt()
+    var max = 0
+    repeat(n) {
+        val x = readln().toInt()
+        if (x % 4 == 0 && x > max) {
+            max = x
+        }
+    }
+    println(max)
 }
