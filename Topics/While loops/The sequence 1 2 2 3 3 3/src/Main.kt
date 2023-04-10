e56bddec1075fd9347f0544169484c56eabe58cc
vitorfloriano <vitorflorianoc@gmail.com>
2023-04-10 19:18:56 -0300

    Just practicing a bit.

--- Topics/While loops/The sequence 1 2 2 3 3 3/src/Main.kt
+++ Topics/While loops/The sequence 1 2 2 3 3 3/src/Main.kt
@@ -1,12 +1,3 @@
 fun main() {
-    var n = readLine()!!.toInt()
-    var i = 1
-    while (i <= n) {
-        var j = 1
-        while (j <= i) {
-            print(i)
-            j++
-        }
-        i++
-    }
+    // put your code here
 }
